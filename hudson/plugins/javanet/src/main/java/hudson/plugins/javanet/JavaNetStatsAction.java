3b9198a3d6f21b731c5de461fac66b8f708388ec
null <null@dev.java.net>
2008-03-11 15:17:36 +0000

    This commit was manufactured by cvs2svn to create tag 'javanet-1_1'.

---
+++ hudson/plugins/javanet/src/main/java/hudson/plugins/javanet/JavaNetStatsAction.java
@@ -0,0 +1,126 @@
+package hudson.plugins.javanet;
+
+import hudson.model.AbstractProject;
+import hudson.model.Action;
+import hudson.model.Hudson;
+import static hudson.plugins.javanet.PluginImpl.DAY;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.stapler.QueryParameter;
+import org.apache.commons.io.FileUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.net.URL;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class JavaNetStatsAction implements Action {
+    /**
+     * Project that owns this action.
+     */
+    public final AbstractProject<?,?> project;
+
+    /**
+     * Java.net project name.
+     */
+    private String projectName;
+
+    private File reportDir;
+
+    public JavaNetStatsAction(AbstractProject<?, ?> project, String projectName) {
+        this.project = project;
+        this.projectName = projectName;
+        this.reportDir = getReportDirectory();
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void scheduleGeneration() {
+        new ReportGenerator(projectName,reportDir).schedule();
+    }
+
+    public String getIconFileName() {
+        return "graph.gif";
+    }
+
+    public String getDisplayName() {
+        return "java.net statistics";
+    }
+
+    public String getUrlName() {
+        return "java.net-stats";
+    }
+
+    public boolean isReportReady() {
+        return new File(reportDir,"index.html").exists();
+    }
+
+    /**
+     * Schedules the re-generation of the report if the report is too old.
+     */
+    public void upToDateCheck() {
+        File indexHtml = new File(reportDir,"index.html");
+        if(!indexHtml.exists() || (System.currentTimeMillis()-indexHtml.lastModified()>7*DAY)) {
+            scheduleGeneration();
+        }
+    }
+
+    public URL getIssueStatsIndexHtml() {
+        return getClass().getClassLoader().getResource("org/jvnet/its/index.html");
+    }
+
+    /**
+     * Directory in which the report is stored.
+     */
+    private File getReportDirectory() {
+        return new File(Hudson.getInstance().getRootDir(), "java.net/issue-tracker-stats/" + projectName);
+    }
+
+    /**
+     * Serves static files in the report directory.
+     */
+    public void doDynamic(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
+        String path = req.getRestOfPath().substring(1);
+
+        // make sure we are not serving anything strange
+        if(!PATH.matcher(path).matches()) {
+            rsp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        rsp.serveFile(req,new File(reportDir,path).toURL());
+    }
+
+    public void doChangeProject(StaplerRequest req, StaplerResponse rsp,@QueryParameter("name") String name) throws IOException, ServletException {
+        projectName = name.trim();
+        reportDir = getReportDirectory();
+        FileUtils.writeStringToFile(getOverrideFile(),projectName,"UTF-8");
+
+        rsp.sendRedirect2(".");
+    }
+
+    /**
+     * File that stores the java.net project name, to manually override
+     * the default project name inference.
+     */
+    private File getOverrideFile() {
+        return new File(project.getRootDir(),"java.net.projectName");
+    }
+
+    /*package*/ static String readOverrideFile(AbstractProject<?,?> project) {
+        try {
+            return FileUtils.readFileToString(new File(project.getRootDir(),"java.net.projectName"),"UTF-8").trim();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private static final Pattern PATH = Pattern.compile("[A-Za-z0-9\\-.]+");
+}
---
+++ hudson/plugins/javanet/src/main/java/hudson/plugins/javanet/StatsProperty.java
@@ -0,0 +1,90 @@
+package hudson.plugins.javanet;
+
+import hudson.model.AbstractProject;
+import hudson.model.Job;
+import hudson.model.JobProperty;
+import hudson.model.JobPropertyDescriptor;
+import hudson.scm.CVSSCM;
+import hudson.scm.SCM;
+import hudson.scm.SubversionSCM;
+import hudson.scm.SubversionSCM.ModuleLocation;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.StaplerRequest;
+
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class StatsProperty extends JobProperty<AbstractProject<?,?>> {
+    public JobPropertyDescriptor getDescriptor() {
+        return DESCRIPTOR;
+    }
+
+    public JavaNetStatsAction getJobAction(AbstractProject<?,?> job) {
+        String jnp = getJavaNetProject(job);
+        if(jnp==null)
+            return null;
+        return new JavaNetStatsAction(job,jnp);
+    }
+
+    private String getJavaNetProject(AbstractProject<?,?> job) {
+        String v = JavaNetStatsAction.readOverrideFile(job);
+        if(v!=null)     return v;
+        return getJavaNetProject(job.getScm());
+    }
+
+    /**
+     * Determines if this SCM is a java.net project,
+     * and if so, return the project name, otherwise null.
+     */
+    private String getJavaNetProject(SCM scm) {
+        if (scm instanceof SubversionSCM) {
+            SubversionSCM sscm = (SubversionSCM) scm;
+            for (ModuleLocation loc : sscm.getLocations()) {
+                Matcher m = SVN_URL.matcher(loc.remote);
+                if(m.matches())
+                    return m.group(1);
+            }
+        }
+        if (scm instanceof CVSSCM) {
+            CVSSCM cscm = (CVSSCM) scm;
+            if(CVS_URL.matcher(cscm.getCvsRoot()).matches()) {
+                StringTokenizer tokens = new StringTokenizer(
+                    cscm.getAllModules(), // TODO: use getAllModulesNormalized
+                    " /\\");
+                return tokens.nextToken();
+            }
+        }
+        return null;
+    }
+
+    private static final Pattern SVN_URL = Pattern.compile("https://[^.]+.dev.java.net/svn/([^/]+)(/.*)?");
+
+    private static final String USERNAME = "([A-Za-z0-9_\\-])+";
+    private static final String HOST = "(.*.dev.java.net|kohsuke.sfbay.*)";
+    private static final Pattern CVS_URL = Pattern.compile(
+        ":pserver:"+USERNAME+"@"+HOST+":/cvs");
+
+    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+
+    public static final class DescriptorImpl extends JobPropertyDescriptor {
+        private DescriptorImpl() {
+            super(StatsProperty.class);
+        }
+
+        public boolean isApplicable(Class<? extends Job> jobType) {
+            return AbstractProject.class.isAssignableFrom(jobType);
+        }
+
+        public String getDisplayName() {
+            return null;
+        }
+
+        public StatsProperty newInstance(StaplerRequest req, JSONObject formData) throws FormException {
+            return new StatsProperty();
+        }
+    }
+}
